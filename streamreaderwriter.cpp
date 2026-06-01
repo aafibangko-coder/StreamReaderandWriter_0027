b961b66bf4f9d5b7c6668348fd92abef8186de50
aafibangko-coder <aafibangko@gmail.com>
2026-06-01 13:47:09 +0700

    membuka file dalam mode menulis

---
+++ streamreaderwriter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    //deklarasi variabel baris untuk menyiapkan data string
+    string baris;
+
+//membuka file dalam mode menulis
+    ofstream outfile;
+    
